735eb02760225eb599c18f31f675afe00f1c362d
ProfessorDurvaldo <durvaldomarques2@gmail.com>
2026-06-09 14:49:39 -0300

    Alteração do nome da loja

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar(props) {
   return (
     <>
       <nav className="navbar">
-        <span className="navbar-logo">Minha Loja</span>
+        <span className="navbar-logo">Loja de Animes</span>
         <span onClick={() => {props.openCart(true)}} className="navbar-cart">Carinho ({props.cartCount})</span>
       </nav>
     </>
